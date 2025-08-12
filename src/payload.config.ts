8024c47dbb24bf6cdf19e524b478733e7be70721
FRQ <frq6692@gmail.com>
2025-08-12 15:59:35 +0500

    all

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,10 +1,9 @@
-import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import path from "path";
 import { buildConfig } from "payload";
 import sharp from "sharp";
 import { fileURLToPath } from "url";
-
 import collections from "./collections";
 
 const filename = fileURLToPath(import.meta.url);
@@ -13,13 +12,15 @@ const dirname = path.dirname(filename);
 export default buildConfig({
   collections,
   editor: lexicalEditor(),
-  secret: process.env.PAYLOAD_SECRET || "",
-    db: mongooseAdapter({
-    url: process.env.DATABASE_URI,
-  }),
-  sharp,
+  secret: process.env.PAYLOAD_SECRET || '',
+
   typescript: {
-    declare: false,
-    outputFile: path.resolve(dirname, "payload-types.ts"),
+    outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
-});
+
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URI || '',
+  }),
+
+  sharp,
+})
