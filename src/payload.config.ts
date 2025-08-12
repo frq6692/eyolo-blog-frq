e9f4012bce11f342c01455146d40815cea28ab1b
FRQ <frq6692@gmail.com>
2025-08-12 15:48:19 +0500

    main changes

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,9 +1,10 @@
-import { postgresAdapter } from "@payloadcms/db-postgres";
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import path from "path";
 import { buildConfig } from "payload";
 import sharp from "sharp";
 import { fileURLToPath } from "url";
+
 import collections from "./collections";
 
 const filename = fileURLToPath(import.meta.url);
@@ -13,10 +14,8 @@ export default buildConfig({
   collections,
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || "",
-  db: postgresAdapter({
-    pool: {
-      connectionString: process.env.DATABASE_URI,
-    },
+    db: mongooseAdapter({
+    url: process.env.DATABASE_URI,
   }),
   sharp,
   typescript: {
